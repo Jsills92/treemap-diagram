b2b64770ff8f05e56f4e59952f12bb92fe5bed8b
Jake Sills <jsills2592@gmail.com>
2025-02-28 11:38:56 -0500

    Set up the promise.all for all three links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import './App.css';
+import * as d3 from "d3";
+import React, { useState, useEffect } from "react";
 
-function App() {
+
+const Treemap = () => {
+  const [data, setData] = useState(null);
+  const [dataset, setDataset] = useState("video-games"); // Default dataset
+  const dataUrls = {
+    "kickstarter": "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json",
+    "movies": "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json",
+    "video-games": "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json"
+  };
+
+  useEffect(() => {
+    Promise.all([
+      fetch(dataUrls["kickstarter"]).then(res => res.json()),
+      fetch(dataUrls["movies"]).then(res => res.json()),
+      fetch(dataUrls["video-games"]).then(res => res.json())
+    ])
+    .then(([kickstarter, movies, videoGames]) => {
+      setData({
+        "kickstarter": kickstarter,
+        "movies": movies,
+        "video-games": videoGames
+      });
+    })
+    .catch(err => console.error("Error fetching data:", err));
+  }, []);
+
   return (
-    <div className="App">
- 
+    <div>
+      <h1 id="title">Treemap Diagram</h1>
+      <p id="description">Visualization of {dataset.replace("-", " ")} data.</p>
+      {/* Navigation for switching datasets */}
+      <nav>
+        {Object.keys(dataUrls).map((key) => (
+          <button key={key} onClick={() => setDataset(key)}>
+            {key.replace("-", " ")}
+          </button>
+        ))}
+      </nav>
+      {/* Treemap SVG will go here */}
+      <svg id="treemap"></svg>
     </div>
   );
-}
+};
 
-export default App;
+
+export default Treemap;
